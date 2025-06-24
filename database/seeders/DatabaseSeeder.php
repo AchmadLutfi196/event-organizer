2338c2e0cad35cd39bffbf0bfadc13f5a5e8581a
AchmadLutfi196 <achmadlutfi196@gmail.com>
2025-06-24 15:32:56 +0700

    fix seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,13 +12,16 @@ class DatabaseSeeder extends Seeder
         // Buat admin user
         User::factory()->create([
             'name' => 'Administrator',
-            'email' => 'admin@inventaris.com',
+            'email' => 'admin@gmail.com',
             'role' => 'admin',
             'is_active' => true
         ]);
 
         // Buat beberapa user biasa
-        User::factory(5)->create([
+        User::factory()->create([
+            'name' => 'User 1',
+            'email' => 'user@gmail.com',
+            'password' => bcrypt('password'),
             'role' => 'user',
             'is_active' => true
         ]);
